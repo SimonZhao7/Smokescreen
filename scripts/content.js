d367f8844dd46e681869c40139635066eb70c0c8
Simon Zhao <68301003+SimonZhao7@users.noreply.github.com>
2023-11-10 13:53:20 -0800

    Add blocking JS

--- scripts/content.js
+++ scripts/content.js
@@ -2,8 +2,24 @@ var target = document.querySelector('body');
 
 var observer = new MutationObserver(function(mutations) {
     mutations.forEach(function(mutation) {
-        console.log('DOM changed: ', mutation);
-        // Your code here
+      const body = document.getElementsByTagName('body')[0];
+      const images = document.getElementsByTagName('img');
+  
+      for (let i = 0; i < images.length; i++) {
+          const { left, top, right, bottom } = images[i].getBoundingClientRect();
+          const width = right - left;
+          const height = bottom - top;
+  
+          const box = document.createElement('div');
+          box.style.width = `${width}px`;
+          box.style.height = `${height}px`;
+          box.style.backgroundColor = 'black';
+          box.style.position = 'absolute';
+          box.style.top = `${top}px`;
+          box.style.left = `${left}px`;
+          body.appendChild(box);
+      }  
+      imgCnt = images.length;
     });
 });
 
