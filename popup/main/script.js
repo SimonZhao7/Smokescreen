967b641d30869a1919a3f3bb1b354c2499bd7bc8
Simon Zhao <68301003+SimonZhao7@users.noreply.github.com>
2023-11-10 22:47:29 -0800

    Add toggle functionality for blocking

--- popup/main/script.js
+++ popup/main/script.js
@@ -1,24 +1,26 @@
 window.addEventListener('load', async () => {
-    const res = await chrome.storage.session.get('toggled');
+    const res = await chrome.storage.local.get('toggled');
     if ('toggled' in res && res['toggled'] === true) {
         btn.innerText = 'ON';   
         btn.className = 'toggle on';
     } else {
-        await chrome.storage.session.set({ toggled: false });
+        await chrome.storage.local.set({ toggled: false });
     }
 })
 
 const btn = document.getElementById('toggle');
 
 btn.addEventListener('click', async () => {
-    const { toggled } = await chrome.storage.session.get('toggled');
+    const { toggled } = await chrome.storage.local.get('toggled');
     if (toggled) {
-        await chrome.storage.session.set({ toggled: false });
+        await chrome.storage.local.set({ toggled: false });
         btn.innerText = 'OFF';
         btn.className = 'toggle off';
     } else {
-        await chrome.storage.session.set({ toggled: true });
+        await chrome.storage.local.set({ toggled: true });
         btn.innerText = 'ON';
         btn.className = 'toggle on';
     }
+    const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
+    await chrome.tabs.sendMessage(tab.id, JSON.stringify({ toggled: !toggled }));
 })
--- scripts/content.js
+++ scripts/content.js
@@ -1,9 +1,10 @@
 var target = document.querySelector('body');
 
-var observer = new MutationObserver(function(mutations) {
-    const images = document.getElementsByTagName("img");
-    const videos = document.getElementsByTagName("video");
-    mutations.forEach(() => {
+var observer = new MutationObserver(async function(mutations) {
+    const res = await chrome.storage.local.get('toggled');
+    if (res['toggled'] !== undefined && res['toggled'] == true) {
+      const images = document.getElementsByTagName("img");
+      const videos = document.getElementsByTagName("video");
       for (let i = Math.max(videos.length, images.length) - 1; i >= 0 ; i--){
         if (i < images.length) images[i].style.display = "none";
         if (i < videos.length) {
@@ -13,7 +14,7 @@ var observer = new MutationObserver(function(mutations) {
           videos[i].controls = false;
         }
       };
-    });
+    }
 });
 
 observer.observe(target, {
@@ -22,3 +23,18 @@ observer.observe(target, {
   characterData: true, // observes changes in 'textContent' of 'target' and descendants
   subtree: true // extends observation to the entire subtree of 'target'
 });
+
+chrome.runtime.onMessage.addListener(msg => {
+  const { toggled } = JSON.parse(msg);
+  const images = document.getElementsByTagName("img");
+  const videos = document.getElementsByTagName("video");
+  for (let i = Math.max(videos.length, images.length) - 1; i >= 0 ; i--){
+    if (i < images.length) images[i].style.display = toggled ? "none" : "block";
+    if (i < videos.length) {
+      videos[i].style.display = toggled ? "none" : "block";
+      if (toggled) videos[i].pause();
+      videos[i].autoplay = !toggled;
+      videos[i].controls = !toggled;
+    }
+  };
+})
