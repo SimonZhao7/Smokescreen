98bafd5a3d57032a372f7223a647b9425c9141ac
Simon Zhao <68301003+SimonZhao7@users.noreply.github.com>
2023-11-10 16:39:05 -0800

    Add button element with preserving state

--- background.js
+++ background.js
@@ -1,3 +1,12 @@
-if (chrome.tabs && chrome.tabs.executeScript) {
-  chrome.tabs.executeScript({code: 'alert("Hello World!");'});
-}
+// chrome.runtime.onInstalled.addListener(async () => {
+//   console.log('ran');
+//   try {
+//     chrome.storage.session.setAccessLevel({ accessLevel: "TRUSTED_AND_UNTRUSTED_CONTEXTS" });
+//     const res = chrome.storage.session.get('toggled');
+//     if (Object.keys(res) == 0) {
+//       chrome.storage.session.set({ toggled: false });
+//     }
+//   } catch {
+//     console.log('error');
+//   }
+// })
