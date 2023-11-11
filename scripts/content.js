0528f2ccb41ae9f693e01d9c44e56869b2691d7e
JiaCheng Liu <85423274+JiaCheng2004@users.noreply.github.com>
2023-11-10 19:09:00 -0800

    Functionality to Block

--- scripts/content.js
+++ scripts/content.js
@@ -2,12 +2,13 @@ var target = document.querySelector('body');
 console.log(target);
 
 var observer = new MutationObserver(function(mutations) {
-    const images = document.getElementsByTagName('img');
-    mutations.forEach(() => { 
-      for (let i = images.length - 1; i >= 0 ; i--){
-        images[i].style.display = "none";
+    const images = document.getElementsByTagName("img");
+    const videos = document.getElementsByTagName("video");
+    mutations.forEach(() => {
+      for (let i = Math.max(videos.length, images.length) - 1; i >= 0 ; i--){
+        if (i < images.length) images[i].style.display = "none";
+        if (i < videos.length) videos[i].style.display = "none";
       };
-      
     });
 });
 
